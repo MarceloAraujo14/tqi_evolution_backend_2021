80664286733cad2f4b6c9b49fc2a2479d616c391
Marcelo Araujo <mbaraujo1406@gmail.com>
2022-01-10 03:05:58 -0300

    Add files via upload

---
+++ userapp/src/main/java/com/userapp/UserappApplication.java
@@ -0,0 +1,13 @@
+package com.userapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserappApplication.class, args);
+	}
+
+}
---
+++ userapp/src/test/java/com/userapp/UserappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
